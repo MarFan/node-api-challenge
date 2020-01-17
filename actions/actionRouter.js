e43f48a82b543470ede8f2c63455ddd0b8b3c6e1
Mr. Jonah <jaitch@gmail.com>
2020-01-17 10:51:33 -0600

    API for actions complete

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -4,22 +4,26 @@ const actionDB = require('../data/helpers/actionModel');
 
 router.get('/:id', (req, res) => {
     actionDB.get(req.params.id)
-        .then(res => res.status(200).json(res))
-        .catch(err => {console.log(err); res.status(500).json({ error: 'There was an error'})})
+        .then(action => res.status(200).json(action))
+        .catch(err => {console.log(err); res.status(500).json({ error: 'There was an error' })})
 });
 
 router.post('/', (req, res) => {
-    actionDB.insert()
-        .then(res => res.status(200).json(res))
-        .catch(err => {console.log(err); res.status(500).json({error: 'Error adding action.'})})
+    actionDB.insert(req.body)
+        .then(action => res.status(200).json(action))
+        .catch(err => {console.log(err); res.status(500).json({error: 'Error adding action.' })})
 })
 
 router.put('/:id', (req, res) => {
-    
+    actionDB.update(req.params.id)
+        .then(action => res.status(200).json(action))
+        .catch(err => {console.log(err); res.status(500).json({ error: 'There was an error updating the action' })})
 })
 
 router.delete('/:id', (req, res) => {
-    
+    actionDB.remove(req.params.id)
+        .then(action => res.status(200).json(action))
+        .catch(err => {console.log(err); res.status(500).json({ error: 'There was an error removing the action' })})
 })
 
 module.exports = router;
