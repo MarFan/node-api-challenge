17b1e8160cce4bc183ea065a88e1b31f80494b42
Mr. Jonah <jaitch@gmail.com>
2020-01-17 10:59:29 -0600

    Actions checking for project

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const actionDB = require('../data/helpers/actionModel');
+const projectDB = require('../data/helpers/projectModel');
 
 router.get('/:id', (req, res) => {
     actionDB.get(req.params.id)
@@ -9,8 +10,16 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    actionDB.insert(req.body)
-        .then(action => res.status(200).json(action))
+    
+    projectDB.get(req.body.project_id)
+        .then(project => {
+            if(!project){
+                res.status(500).json({ error: 'Sorry, can\' find that project. Try again.'})
+            }
+            actionDB.insert(req.body)
+                .then(action => res.status(200).json(action))
+                .catch(err => {console.log(err); res.status(500).json({error: 'Error adding action.' })})
+        })
         .catch(err => {console.log(err); res.status(500).json({error: 'Error adding action.' })})
 })
 
